e9e6363d690c379203c62d76e2e746e87c5057e4
Jocelyn Wu <b09704062@ntu.edu.tw>
2024-05-20 15:19:40 +0800

    24th

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     #message = TextSendMessage(text=event.message.text)
-    #web = requests.get('https://script.google.com/macros/s/AKfycbzX3R7MRV4rvd1GW_MyFLG7faiD0ATWpXMRy_MzKtjN2NelngTr-r0iaq_fGvbAkdnCHw/exec')
-    message = TextSendMessage(text = "QQQ")
+    web = requests.get('https://script.google.com/macros/s/AKfycbzX3R7MRV4rvd1GW_MyFLG7faiD0ATWpXMRy_MzKtjN2NelngTr-r0iaq_fGvbAkdnCHw/exec')
+    message = TextSendMessage(text = web.json())
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
