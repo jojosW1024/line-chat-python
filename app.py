d1de85bc6d0ed1c7550c7954822555a865fd0bcc
Jocelyn Wu <b09704062@ntu.edu.tw>
2024-05-20 14:37:13 +0800

    try keelung 20th times

--- app.py
+++ app.py
@@ -32,8 +32,9 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    #web = requests.get('https://script.google.com/macros/s/AKfycbzX3R7MRV4rvd1GW_MyFLG7faiD0ATWpXMRy_MzKtjN2NelngTr-r0iaq_fGvbAkdnCHw/exec')
+    #message = TextSendMessage(text=event.message.text)
+    web = requests.get('https://script.google.com/macros/s/AKfycbzX3R7MRV4rvd1GW_MyFLG7faiD0ATWpXMRy_MzKtjN2NelngTr-r0iaq_fGvbAkdnCHw/exec')
+    message = TextSendMessage(text = web.json()[1])
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
